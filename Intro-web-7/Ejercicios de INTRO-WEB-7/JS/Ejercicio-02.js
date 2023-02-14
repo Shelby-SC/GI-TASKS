9e66e7b8b076c05ceb553d2998e37d47d62e7f6d
Shelby-SC <shelby.sabido@gmail.com>
2023-02-14 07:59:31 -0600

    Actualizacion ejercicios

---
+++ Intro-web-7/Ejercicios de INTRO-WEB-7/JS/Ejercicio-02.js
@@ -0,0 +1,8 @@
+
+function funfact(){
+    alert("We don't care about seasons, we only have two. Hot and very hot.")
+}
+
+function funfact2(){
+    alert("Here you have another one: Being far from the beach. You don't need to wait for a special season or vacation, you can go to the beach at anytime.")
+}
