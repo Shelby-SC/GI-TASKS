3b689bb0e9f6e608300d96644890a199b6710b3b
Shelby-SC <shelby.sabido@gmail.com>
2023-02-20 06:22:24 -0600

    extra

---
+++ T-PL/practscript.js
@@ -0,0 +1,13 @@
+
+
+for (i=1; i <= 100; i++) {
+        if (i % 3 == 0 && i % 5 == 0) {
+            console.log("FuzzBuzz");
+        } else if (i % 3 == 0) {
+            console.log("Fuzz");
+        } else if (i % 5 == 0) {
+            console.log("Buzz");
+        } else {
+            console.log(i);
+        }
+} 
